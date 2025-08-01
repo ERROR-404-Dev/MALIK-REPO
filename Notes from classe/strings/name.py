1249deba2ffe293f1948a0dc59eead1403d6c519
Malik-Mangue <erro404.dev@gmail.com>
2025-08-01 21:57:25 +0200

    criando o programa name
    crei um programa name para testar o title(), upper() and lower() para modificar os nomes na string

--- Notes from classe/strings/name.py
+++ Notes from classe/strings/name.py
@@ -1,5 +1,9 @@
 #Para colocar em letra maiscula cada primeira letra
-name = 'Ada lovalace'
+name = 'Ada loValacE'
 print(name.title())
 
-#Para colocar
+#Para colocar tudo maísculo
+print(name.upper())
+
+#Para colocar letras todas minúsculas
+print(name.lower())
