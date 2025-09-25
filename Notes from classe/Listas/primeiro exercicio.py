4f0aff66aa432017987d79326285e3b51a43e51a
Malik-Mangue <erro404.dev@gmail.com>
2025-09-25 19:10:04 +0200

    criacao de lista

---
+++ Notes from classe/Listas/primeiro exercicio.py
@@ -0,0 +1,8 @@
+Nomes = ['Evgueny', 'Ewen', 'Divan','Salomao','David','Mario']
+
+print(f'Ola {Nomes[0]},tudo bem')
+print(f'Ola {Nomes[1]},como estas')
+print(f'Hi {Nomes[2]},tudo bem')
+print(f'kmk, {Nomes[3]},como vai isso')
+print(f'bonjour {Nomes[4]}, como çava')
+print(f'Hello {Nomes[5]}, how is it going')
